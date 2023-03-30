3e9e8695a051a620713470fe1a46be2d012a6abf
M. Mukhtarul Lathief <55247804+lathief@users.noreply.github.com>
2023-03-31 06:22:13 +0700

    Update model.go

--- model/model.go
+++ model/model.go
@@ -9,6 +9,14 @@ type Book struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
+type BookResponse struct {
+	ID        int       `json:"id"`
+	Name_Book string    `json:"name_book"`
+	Author    string    `json:"author"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
 type BookRequest struct {
 	Name_Book string `json:"name_book"`
 	Author    string `json:"author"`
